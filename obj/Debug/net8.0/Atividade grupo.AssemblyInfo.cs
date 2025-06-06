27def757edd5522110027cb97514f6ab54784ecc
Luis Sampaio <luishsampaioviera@gmail.com>
2025-06-06 18:03:58 -0300

    ATT C#

--- obj/Debug/net8.0/Atividade grupo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Atividade grupo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade grupo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66a79d23f288efeea8161685c8c53a661a79ad31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3cd298bf50da6380f15c655279e460b332cb070")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade grupo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade grupo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
